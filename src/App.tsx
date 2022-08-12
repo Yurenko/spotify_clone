f154578093be9f947e3046039c4986d0ecefe161
Yurenko <didenkoyura225@gmail.com>
2022-08-12 14:51:18 +0300

    Logg In

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { accessUrl, getTokenFromResponse } from "./Auth/Auth";
 
 function App() {
+  const [token, setToken] = useState("");
+  useEffect(() => {
+    const hash = getTokenFromResponse();
+    window.location.hash = "";
+    let _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href={accessUrl}>Log in with Spotify</a>
+      {token}
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react'
+import styles from './NavBar.module.scss'
+
+const NavBar: FC = () => {
+    return <div>NavBar</div>
+}
+
+export default NavBar
